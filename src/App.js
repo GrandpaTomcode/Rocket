1f5f6446187a78376ffee9914594dc3a10a289e6
Tom <tomhornbuckle1@gmail.com>
2018-12-12 23:52:31 +0000

    Commiting the files do the repository.

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import './App.css';
+import Home from './components/home'
+import Backend from './components/backend'
+import Error from './components/error'
+import Navigation from './components/Navigation'
+
+
+
+class App extends Component {
+
+  render() {
+
+    return (
+
+      <BrowserRouter>
+        <div>
+          <Navigation />
+          <Switch>
+            <Route path='/' component={Home} exact />
+            <Route path='/backend' component={Backend} />
+            <Route component={Error} />
+          </Switch>
+        </div>
+
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/backend.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { fire } from './config'
+
+export class Backend extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            id: '',
+            title: 'First State',
+            body: 'First State'
+        }
+
+    }
+
+    componentDidMount() {
+        const rootRef = fire.database().ref()
+        const post = rootRef.child('posts').orderByKey()
+        post.once('value', snap => {
+            snap.forEach((childNodes) => {
+                this.setState({
+                    id: childNodes.key,
+                    title: childNodes.val().title,
+                    body: childNodes.val().body
+                })
+                console.log(childNodes.val().title)
+                console.log(childNodes.val().body)
+            })
+        })
+    }
+    render() {
+        return (
+            <div>
+                <p> Backend </p>
+                <h1>Title: {this.state.title} </h1>
+                <h1>Body: {this.state.body} </h1>
+            </div>
+        )
+    }
+}
+
+export default Backend
