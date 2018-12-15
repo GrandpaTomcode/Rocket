83f301f4bc43318c97ef2d2b4011e9b358ac6eff
Tom <tomhornbuckle1@gmail.com>
2018-12-15 12:59:44 +0000

    Slightly closer to loop through list of posts

--- src/components/home.js
+++ src/components/home.js
@@ -1,57 +1,70 @@
 import React from 'react'
 import Navigation from './Navigation'
 import { fire } from './config'
+import '../css/App.css'
 
 class Home extends React.Component {
     constructor() {
         super()
         this.state = {
-            id: '',
-            title: 'First State',
-            body: 'First State',
-            date: '13/12/2018'
+            posts:
+            {
+                id: '',
+                title: 'First State',
+                body: 'First State',
+                date: '13/12/2018'
+            }
         }
     }
 
     componentDidMount() {
         const rootRef = fire.database().ref()
         const post = rootRef.child('posts').orderByKey()
+        var connectedRef = fire.database().ref('.info/connected')
+        connectedRef.on('value', function (snap) {
+            if (snap.val() === true) {
+                console.log('connected')
+            } else {
+                console.log('not connected')
+            }
+        })
         post.once('value', snap => {
             snap.forEach(childNodes => {
                 this.setState({
+                    id: childNodes.key,
                     title: childNodes.val().title,
                     body: childNodes.val().body,
                     date: childNodes.val().date
                 })
+                console.log(this.state.id)
+                console.log(this.state.title)
+                console.log(this.state.date)
+                console.log(this.state.body)
             })
-            var connectedRef = fire.database().ref('.info/connected')
-            connectedRef.on('value', function(snap) {
-                if (snap.val() === true) {
-                    console.log('connected')
-                } else {
-                    console.log('not connected')
-                }
-            })
+
         })
     }
     render() {
+        const listOfPosts = Object.keys(this.state.posts).map(key =>
+            <div className="posts">
+                <h1 key={key.id}>{this.state.title}</h1>
+                <p key={key.id}>{this.state.date}</p>
+                <p key={key.id}>{this.state.body}</p>
+            </div>
+        )
+        console.log(listOfPosts)
         return (
             <div>
                 <div className="header">
                     <h1 className="headerTitle">Rocket</h1>
                 </div>
-                <div>
-                    <Navigation />
-                    <div>
-                        <div className="posts">
-                            <h1>{this.state.title}</h1>
-                            <p>{this.state.date}</p>
-                            <p>{this.state.body}</p>
-                        </div>
-                    </div>
-                </div>
+                <Navigation />
+                <div>{listOfPosts}</div>
             </div>
-        )
+
+        );
+
+
     }
 }
 
